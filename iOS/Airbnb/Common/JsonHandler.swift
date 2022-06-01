65600cec959f51935bffc7c92303f500fdc536cf
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-01 11:33:13 +0900

    [junu0516/airbnb#27] Chore: JsonHandler 별도 파일로 분리, JsonHandlable 프로토콜 추가

---
+++ iOS/Airbnb/Common/JsonHandler.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+protocol JsonHandlable {
+    func convertJsonToObject<T:Decodable>(from data: Data, to targetType: T.Type) -> T?
+    func convertObjectToJson<T:Encodable>(from object: T) -> Data?
+}
+
+struct JsonHandler {
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
+    func convertJsonToObject<T: Decodable>(from data: Data, to targetType: T.Type) -> T? {
+        return try? decoder.decode(T.self, from: data)
+    }
+    
+    func convertObjectToJson<T: Encodable>(from object: T) -> Data? {
+        return try? encoder.encode(object)
+    }
+}
--- iOS/Airbnb/RoomDetail/RoomDetailRepository.swift
+++ iOS/Airbnb/RoomDetail/RoomDetailRepository.swift
@@ -1,20 +1,6 @@
 import Foundation
 import OSLog
 
-struct JsonHandler {
-    private let encoder = JSONEncoder()
-    private let decoder = JSONDecoder()
-    
-    func convertJsonToObject<T: Decodable>(from data: Data, to targetType: T.Type) -> T? {
-        return try? decoder.decode(T.self, from: data)
-    }
-    
-    func convertObjectToJson<T: Encodable>(from object: T) -> Data? {
-        return try? encoder.encode(object)
-    }
-}
-
-
 struct RoomDetailRepository {
     private let networkHandler = NetworkHandler()
     private let jsonHandler = JsonHandler()
