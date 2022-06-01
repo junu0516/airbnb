51411e5b140a3f8fb87620bc97e685cf5191a148
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-01 12:18:30 +0900

    [junu0516/airbnb#27] Feat: RoomPositionMapRepository 구현(Repository 계층)

---
+++ iOS/Airbnb/RoomPositionMap/RoomPositionMapRepository.swift
@@ -0,0 +1,30 @@
+import Foundation
+import OSLog
+
+struct RoomPositionMapRepository {
+    
+    private let logger = Logger()
+    private let networkHandler: NetworkHandlable
+    private let jsonHandler: JsonHandlable
+    
+    init(networkHandler: NetworkHandlable, jsonHandler: JsonHandlable) {
+        self.networkHandler = networkHandler
+        self.jsonHandler = jsonHandler
+    }
+    
+    func fetch(completion: @escaping (RoomPositionInfo) -> Void) {
+        networkHandler.request(endPoint: .list,
+                               method: .get,
+                               contentType: .json,
+                               body: nil) { result in
+            switch result {
+            case .success(let data):
+                if let decodedData = jsonHandler.convertJsonToObject(from: data, to: RoomPositionInfo.self) {
+                    completion(decodedData)
+                }
+            case .failure(let error):
+                logger.error("\(error.localizedDescription)")
+            }
+        }
+    }
+}
