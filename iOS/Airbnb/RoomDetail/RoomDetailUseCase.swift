96fcb4384654d43ef0e6a5787921c92aa3748aed
Jinsujin <sylpid003@gmail.com>
2022-06-02 22:09:39 +0900

    [junu0516/airbnb#32] Feat: Profile 이미지 불러와서 화면에 보여주기

--- iOS/Airbnb/RoomDetail/RoomDetailUseCase.swift
+++ iOS/Airbnb/RoomDetail/RoomDetailUseCase.swift
@@ -7,6 +7,7 @@ class RoomDetailUseCase {
     private (set)var roomDetail: Observable<RoomDetail> = Observable<RoomDetail>(RoomDetail())
     
     private (set)var image: Observable<Data> = Observable<Data>(Data())
+    private (set)var profileImage: Observable<Data> = Observable<Data>(Data())
     
     init(roomId: UniqueID, repository: RoomDetailRepository) {
         self.repository = repository
@@ -20,6 +21,10 @@ class RoomDetailUseCase {
             // 임시로 imageUrl 배열에서 url 하나만 꺼내서 요청하도록 처리 (추후 연쇄 요청으로 수정 필요)
             self?.repository.fetchImage(imageUrl: responseData.images[0]) { [weak self] responseImage in
                 self?.image.value = responseImage
+            }
+            
+            self?.repository.fetchImage(imageUrl: responseData.profileOfHost) { [weak self] responseImage in
+                self?.profileImage.value = responseImage
             }
         }
     }
