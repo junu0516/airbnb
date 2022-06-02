28ecfe840203d5e4f8c5c2986115504d7e6ebf7c
Jinsujin <sylpid003@gmail.com>
2022-06-02 15:52:17 +0900

    [junu0516/airbnb#31] Feat: 검색결과 Entity 에 totalPrice 속성 추가

--- iOS/Airbnb/RoomSearch/RoomSearchUseCase.swift
+++ iOS/Airbnb/RoomSearch/RoomSearchUseCase.swift
@@ -35,10 +35,10 @@ struct SearchCondition {
 struct RoomSearchResultFactory {
     
     private (set)var roomList: [Room] = [
-        .init(roomId: 1, roomName: "Spacious and Comfortable cozy house #4", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%801.PNG", pricePerDay: 32000, isWished: true, averageOfStar: 3.8, numberOfReviews: 125),
-        .init(roomId: 2, roomName: "cozy house", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%802.PNG", pricePerDay: 47000, isWished: false, averageOfStar: 5.0, numberOfReviews: 5),
-        .init(roomId: 3, roomName: "sunset house", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%803.PNG", pricePerDay: 36600, isWished: false, averageOfStar: 2.3, numberOfReviews: 10),
-        .init(roomId: 4, roomName: "Green View", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%804.PNG", pricePerDay: 41000, isWished: false, averageOfStar: 4.1, numberOfReviews:62),
+        .init(roomId: 1, roomName: "Spacious and Comfortable cozy house #4", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%801.PNG", pricePerDay: 32000, totalPrice: 189000, isWished: true, averageOfStar: 3.8, numberOfReviews: 125),
+        .init(roomId: 2, roomName: "cozy house", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%802.PNG", pricePerDay: 47000, totalPrice: 242000, isWished: false, averageOfStar: 5.0, numberOfReviews: 5),
+        .init(roomId: 3, roomName: "sunset house", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%803.PNG", pricePerDay: 36600, totalPrice: 110000, isWished: false, averageOfStar: 2.3, numberOfReviews: 10),
+        .init(roomId: 4, roomName: "Green View", thumbnailImage: "https://sally-airbnb.s3.ap-northeast-2.amazonaws.com/1-%EC%A7%84%EB%B6%80%EB%A9%B4-%EC%9D%80%EC%A7%804.PNG", pricePerDay: 41000, totalPrice: 153000, isWished: false, averageOfStar: 4.1, numberOfReviews:62),
     ]
 }
 
@@ -49,6 +49,7 @@ struct Room {
     let roomName: String
     let thumbnailImage: String //url
     let pricePerDay: Int
+    let totalPrice: Int
     let isWished: Bool
     let averageOfStar: Float
     let numberOfReviews: Int
