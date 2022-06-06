9cace94b25e5ac8335d1ba916303c9f40f711d0f
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-07 00:09:53 +0900

    [junu0516/airbnb#39] Ref: Reservation Encodable 채택하는 로직 Reservation.swift 파일로 이동

--- iOS/Airbnb/Common/String+Enxtension.swift
+++ iOS/Airbnb/Common/String+Enxtension.swift
@@ -13,35 +13,3 @@ extension String {
         }
     }
 }
-
-extension Reservation: Encodable {
-    
-    enum CodingKeys: String, CodingKey {
-        case checkIn
-        case checkOut
-        case cleaningPrice
-        case discountedPricePerWeek
-        case fee
-        case guestAmount
-        case guestId
-        case priceForOneDay
-        case roomId
-        case tax
-        case totalPrice
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.checkInDate.toDate(format: "yyyy-MM-dd"), forKey: .checkIn)
-        try container.encode(self.checkOutDate.toDate(format: "yyyy-MM-dd"), forKey: .checkOut)
-        try container.encode(self.cleaningPrice, forKey: .cleaningPrice)
-        try container.encode(self.discountedPricePerWeek, forKey: .discountedPricePerWeek)
-        try container.encode(self.fee, forKey: .fee)
-        try container.encode(self.guestsCount, forKey: .guestAmount)
-        try container.encode(1, forKey: .guestId)
-        try container.encode(self.priceForOneDay, forKey: .priceForOneDay)
-        try container.encode(self.tax, forKey: .tax)
-        try container.encode(self.totalPrice, forKey: .totalPrice)
-    }
-}
-
