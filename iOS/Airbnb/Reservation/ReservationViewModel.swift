caf009301645924ef88f46cc4e8780022e5630bb
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-07 21:31:28 +0900

    [junu0516/airbnb#54] Ref: Reservation 내부에서 ReservationPrice 배열 생성하는 로직을 뷰모델로 이동

--- iOS/Airbnb/Reservation/ReservationViewModel.swift
+++ iOS/Airbnb/Reservation/ReservationViewModel.swift
@@ -1,9 +1,10 @@
 import Foundation
 
-final class ReservationViewModel {
+struct ReservationViewModel {
     
     private (set)var reservation = Observable<Reservation>(Reservation())
     private (set)var reservationPrices = Observable<[ReservationPrice]>([])
+    //alert message
     private let repository: ReservationRepository
     
     init(reservationRepository repository: ReservationRepository) {
@@ -16,5 +17,25 @@ final class ReservationViewModel {
                print("예약 요청")
             }
         }
+    }
+    
+    func generateReservationPrices() -> [ReservationPrice] {
+        let checkInDate = reservation.value.checkInDate
+        let checkOutDate = reservation.value.checkOutDate
+        let interval = checkOutDate.getDateInterval(from: checkInDate)
+        
+        let priceForOneDay = reservation.value.priceForOneDay
+        let discountedPricePerWeek = reservation.value.discountedPricePerWeek
+        let cleaningCost = reservation.value.cleaningPrice
+        let serviceFee = reservation.value.fee
+        let accomodationFee = reservation.value.tax
+        
+        return [
+            .init(title: .originalPrice(price: "\(priceForOneDay.toDecimalString() ?? "")", dates: "\(interval)"), value: priceForOneDay*interval),
+            .init(title: .weeklyDiscount, value: discountedPricePerWeek),
+            .init(title: .cleaningCost, value: cleaningCost),
+            .init(title: .serviceFee, value: serviceFee),
+            .init(title: .accomodationFee, value: accomodationFee)
+        ]
     }
 }
