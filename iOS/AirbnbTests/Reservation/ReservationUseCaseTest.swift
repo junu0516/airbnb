ac7f9cd9485c418f74feac8647385f2bd1c2b355
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-08 20:29:24 +0900

    [junu0516/airbnb#54] Test: ReservationUseCaseTest에 예약 관런 단위테스트 추가, 기존의 ReservationViewModelTest 제거

---
+++ iOS/AirbnbTests/Reservation/ReservationUseCaseTest.swift
@@ -0,0 +1,70 @@
+import XCTest
+@testable import Airbnb
+
+class ReservationUseCaseTest: XCTestCase {
+
+    private var useCase: ReservationUseCase!
+    private var repository: ReservationRepository!
+    private var networkManager: MockNetworkServiceManager!
+    
+    override func setUp(){
+        super.setUp()
+        
+        let checkInDate = DateComponents(year: 2022, month:5, day:17)
+        let checkOutDate = DateComponents(year: 2022, month:6, day:4)
+        let reservation = Reservation(checkInDate: checkInDate, checkOutDate: checkOutDate, guestsCount: 3, priceForOneDay: 70358)
+        
+        self.networkManager = MockNetworkServiceManager()
+        self.repository = ReservationRepository(networkHandler: networkManager, jsonHandler: JsonHandler())
+        self.useCase = ReservationUseCase(reservationRepository: repository, reservation: reservation)
+    }
+    
+    /*
+       ReservationPrice 객체 배열 생성 관련 테스트
+     */
+    func test_generating_prices() {
+        //우선 Observable<[ReservationPrice]>의 value 속성의 Nil 여부 확인
+        XCTAssertNotNil(self.useCase.reservation.value)
+        //빈 배열이 들어있는지 value 속성에 있는 배열 인스턴스의 길이 0 이하인지 확인
+        XCTAssertLessThanOrEqual(self.useCase.reservationPrices.value.count, 0)
+        
+        //generateReservationPrices() 메소드 호출 후, value 속성에 있는 배열 인스턴스의 길이가 1 이상인지 확인
+        let prices = self.useCase.generateReservationPrices()
+        self.useCase.reservationPrices.value = prices
+        XCTAssertGreaterThan(self.useCase.reservationPrices.value.count, 1)
+    }
+    
+    /*
+        예약 POST 요청 관련 테스트
+     */
+    
+    //예약 성공을 가정하고 테스트
+    func test_sending_reservation_request_success() throws {
+        
+        //최초에는 reservationResultFlag의 값이 nil이어야 함
+        XCTAssertNil(useCase.reservationResultFlag?.value)
+        
+        networkManager.shouldFail = false
+        useCase.sendReservationRequest()
+        XCTAssertNotNil(useCase.reservationResultFlag?.value)
+        
+        let result = try XCTUnwrap(useCase.reservationResultFlag?.value)
+        XCTAssertEqual(result, true)
+    }
+    
+    //예약 실패를 가정하고 테스트
+    func test_sending_reservation_request_failure() throws {
+        
+        //최초에는 reservationResultFlag의 값이 nil이어야 함
+        XCTAssertNil(useCase.reservationResultFlag?.value)
+        
+        //예약 실패를 가정하고 테스트
+        networkManager.shouldFail = true
+        useCase.sendReservationRequest()
+        XCTAssertNotNil(useCase.reservationResultFlag?.value)
+        
+        let result = try XCTUnwrap(useCase.reservationResultFlag?.value)
+        XCTAssertEqual(useCase.reservationResultFlag?.value, false)
+    }
+    
+}
