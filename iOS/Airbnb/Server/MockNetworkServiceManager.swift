10f70a55bbc6d2aefe0a16d02e7168f388630796
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-08 20:28:41 +0900

    [junu0516/airbnb#54] Feat: MockNetworkServiceManager 구현, 기존의 MockNetworkHandler 제거

---
+++ iOS/Airbnb/Server/MockNetworkServiceManager.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+final class MockNetworkServiceManager: NetworkService {
+    
+    //Mock 객체 내부에서 사용할 사용자 정의 에러 열거형
+    enum TestError: Error, CustomStringConvertible {
+        case mockDataNotLoaded
+        case expectedError
+        
+        var description: String {
+            switch self {
+            case .mockDataNotLoaded:
+                return "Mock Data Not Loaded"
+            case .expectedError:
+                return "Expected Error"
+            }
+        }
+    }
+    
+    //미리 성공, 실패 여부를 결정
+    var shouldFail: Bool = true
+    var mockResponseData: Data?
+    
+    //필요 시 로컬에 미리 저장된 json 데이터를 가지고옴
+    func loadMockData() {
+        self.mockResponseData = Data()
+    }
+    
+    func request(endPoint: EndPoint,
+                 body: Data?,
+                 completion: @escaping (Result<Data, Error>) -> Void) {
+        
+        switch shouldFail {
+        case false:
+            //성공한 응답가지고 클로저 호출
+            completion(.success(Data()))
+        case true:
+            //실패한 응답 가지고 클로저 호출
+            completion(.failure(TestError.expectedError))
+        }
+    }
+}
