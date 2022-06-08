2db96772f1331569255963aa0d56125ff9d8c7fa
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-09 03:09:02 +0900

    [junu0516/airbnb#37] Ref: RoomPosition의 placeId 속성 제거, PositionSearchUseCase에서 placeId 사용하는 부분 제거
    - placeId를 가지고 좌표 정보를 검색하지 않아도 되기 때문에 모두 제거해야 함

--- iOS/Airbnb/Entity/RoomPosition.swift
+++ iOS/Airbnb/Entity/RoomPosition.swift
@@ -2,19 +2,17 @@ import Foundation
 
 
 struct RoomPosition {
+
     let address: String
-    let placeId: String
     private let category: RoomPositionCategory // 주소가 포함된 시,군,구
     
     init(address: String) {
         self.address = address
-        self.placeId = ""
         self.category = RoomPositionCategory(categoryLiteral: "서울시")
     }
     
     init(address: String, placeId: String) {
         self.address = address
-        self.placeId = placeId
         self.category = RoomPositionCategory(categoryLiteral: "서울시")
     }
 }
