3b3fd509eff403239ed74de4fdbddab0d3554fa8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-06-13 02:05:04 +0900

    [junu0516/airbnb#72] Ref: GET요청시 QueryString을 추가할 수 있도록 NetworkServiceManager 수정

--- iOS/Airbnb/Server/NetworkServiceManager.swift
+++ iOS/Airbnb/Server/NetworkServiceManager.swift
@@ -9,24 +9,47 @@ protocol NetworkService {
 struct NetworkServiceManager: NetworkService {
     
     private let logger = Logger()
+    private let converter: ObjectConvertible = CustomConverter()
     private var serializer: DataResponseSerializer {
         return .init(emptyResponseCodes: Set([201]))
     }
     
+    private func headers(_ headers: [String: String]) -> HTTPHeaders {
+        return HTTPHeaders(headers)
+    }
+    
+    private func httpMethod(_ method: HttpMethod) -> HTTPMethod {
+        return HTTPMethod(rawValue: method.description)
+    }
+    
+    private func parameters(_ body: Data?) -> [String:Any]? {
+        guard let body = body else { return nil}
+        return converter.convertJsonToDictionary(from: body)
+    }
+
+    private func encoding(_ method: HttpMethod) -> ParameterEncoding {
+        switch method {
+        case .get:
+            return URLEncoding.queryString
+        case .post:
+            return JSONEncoding.default
+        }
+    }
+    
     func request(endPoint: EndPoint, body: Data?, completion: @escaping (Result<Data, Error>) -> Void) {
-        
-        guard var request = try? URLRequest(url: endPoint.url,
-                                            method: HTTPMethod(rawValue: "\(endPoint.method)"),
-                                            headers: HTTPHeaders(endPoint.headers)) else { return }
-        request.httpBody = body
-        
-        AF.request(request)
+
+        AF.request(endPoint.url,
+                   method: httpMethod(endPoint.method),
+                   parameters: parameters(body),
+                   encoding: encoding(endPoint.method),
+                   headers: headers(endPoint.headers))
         .validate(statusCode: 200..<300)
         .response(responseSerializer: serializer) { response in
             switch response.result {
             case .success(let data):
                 completion(.success(data))
             case .failure(let error):
+                print(response.request)
                 logger.error("\(error.localizedDescription)")
                 completion(.failure(error))
             }
